ed62935c452dbae96cc10b1c3dd2543adde995bb
Samuel Curry <samc735@gmail.com>
2018-04-23 23:54:11 -0400

    port 3001

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.get('/:encoded_id', function(req, res){
 
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(3001, function(){
   console.log('Server listening on port 3000');
 });
