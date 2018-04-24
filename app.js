9b7e2c1c6a403c7cce1f07478026bf8da34305ea
Samuel Curry <samc735@gmail.com>
2018-04-24 00:30:27 -0400

    console.logs

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var config = require('./config');
 var base58 = require('./base58.js');
-
-// grab the url model
 var Url = require('./models/url');
 
 mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
@@ -23,28 +21,20 @@ app.get('/', function(req, res){
 app.post('/api/shorten', function(req, res){
   var longUrl = req.body.url;
   var shortUrl = '';
-
-  // check if url already exists in database
   Url.findOne({long_url: longUrl}, function (err, doc){
     if (doc){
       shortUrl = config.webhost + base58.encode(doc._id);
-      console.log(`The shortURL is: ` + shortUrl);
-      // the document exists, so we return it without creating a new entry
       res.send({'shortUrl': shortUrl});
     } else {
-      // since it doesn't exist, let's go ahead and create it:
       var newUrl = Url({
         long_url: longUrl
       });
-
-      // save the new link
       newUrl.save(function(err) {
         if (err){
           console.log(err);
         }
 
         shortUrl = config.webhost + base58.encode(newUrl._id);
-        console.log(`The shortURL is: ` + shortUrl);
         res.send({'shortUrl': shortUrl});
       });
     }
@@ -62,10 +52,8 @@ app.get('/:encoded_id', function(req, res){
   // check if url already exists in database
   Url.findOne({_id: id}, function (err, doc){
     if (doc) {
-      console.log(`The longURL is: ` + doc.long_url);
       res.redirect(doc.long_url);
     } else {
-      console.log(`The webhost is: ` + config.webhost);
       res.redirect(config.webhost);
     }
   });
@@ -73,5 +61,4 @@ app.get('/:encoded_id', function(req, res){
 });
 
 var server = app.listen(3001, function(){
-  console.log('Server listening on port 3001');
 });
